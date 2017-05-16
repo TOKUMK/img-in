3eea17f9b593174763681ff3accd5385c31d1447
TOKUMK <jcaseyire@gmail.com>
2017-05-16 13:34:15 +0700

    add function code for file upload using formiddable middleware

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 var express     = require('express'),
+    bodyParser  = require('body-parser'),
     multer      = require('multer'),
     MongoClient = require('mongodb').MongoClient,
     assert      = require('assert'),
-    //bodyParser  = require('body-parser'),
-    formidable  = require('express-formidable');
-
-	//var multipart = require('connect-multiparty');
+    util        = require('util'),
+    formidable  = require('formidable');
 
 var app = express();
 
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
-app.use(formidable.parse());
+//app.use(formidable.parse());
 
-//app.use(multer);
+// Handle use case for file uploading.
+// TODO: Persist file data.
+app.post("/upload", function (req, res) {
+    //console.log(req.body.user.name)
 
-//app.use(require('connect').bodyParser());
-//app.use(bodyParser.json()); // to support JSON bodies
-//app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+    var form = new formidable.IncomingForm();
+    var cat, filename;
 
-//app.use(bodyParser.json());         
-//app.use(bodyParser.urlencoded({ extended: true }));                                
+   // form.parse(req,);
 
-
-//  -------------------------------------------------
-
-// file storage - multer.
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now() + 'CAT_TYPE');
-  }
-});
-
-var upload = multer({ storage : storage}).single('userPhoto');
-
-//  -------------------------------------------------
+    form.parse(req, function (req,fields, err){
+        filename = fields.filename;
+        cat = fields.cat;
+        console.log("cattype = " + fields.cat); 
+        console.log("filename = " + fields.filename); 
+    });
 
 
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
 
-app.post("/", function (req, res) {
-    console.log(req.body.user.name)
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.end("File is uploaded");
+
+
 });
 
 
@@ -50,49 +47,14 @@ app.get('/',function(req,res){
       res.sendFile(__dirname + "/index.html");
 });
 
-
-// handle post data - file upload and get.cat
-app.post('/api/photo', function(req,res){
-    console.log("request to upload image recvied.. upload in progress.");
-    
-    console.log("req.catype() = " + req.cat);
-    console.log("req.catype() = " + req.body.cat);
-    console.log("req.catype() = " + req.body.txtval);
-
-
-    // handle file persistence to disk.
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-
-
-
-	/* TOTEST
- 	var files = req.files;
-    if (files) {//Checks to see if any attached files
-        files.forEach(function (file) {
-            console.log('FILE');
-            console.log(JSON.stringify(file));//contents of file
-            console.log(JSON.stringify(file.filename));
-        });
-    }
-    console.log(req.body);
-    res.sendFile(__dirname + "/resources/JSONStructures/genericResponse.json"); */
-
-
-
+app.get('/hello',function(req,res){
+      res.sendFile(__dirname + "/hello.html");
 });
 
 
-app.listen(3000);
-
-
-
-
-
+var listener = app.listen(3000, function(){
+	console.log('listening on port ' + listener.address().port);
+});
 
 
 
