fffc092e031ec6d40b9a1cbeeda101ef59b03362
TOKUMK <jcaseyire@gmail.com>
2017-06-15 14:41:59 +0700

    updating persistence functionality

--- index.js
+++ index.js
@@ -3,10 +3,117 @@ var express     = require('express'),
     assert      = require('assert'),
     util        = require('util'),
     formidable  = require('formidable');
+    mongoose    = require('mongoose');
+    User        = require('./User.model');
 
 var app = express();
 
+var db = 'mongodb://localhost/example';
+
+mongoose.connect(db);
+
 app.use('/img', express.static(__dirname + '/img'));
+
+var listener = app.listen(3000, function(){
+    console.log('listening on port ' + listener.address().port);
+
+});
+
+// -------------------- routes start here -----------------------------
+
+
+
+/*
+
+        Routes for user registration and user data.
+
+*/
+
+// return the view page for register
+app.get('/register',function(req,res){
+      res.sendFile(__dirname + "/register.html");
+});
+
+
+
+// handle user registration 
+// form validation should be done on both front and back end.
+app.post('/register',function(req,res){
+
+    var form = new formidable.IncomingForm();
+    //var username, email, password, repassword;
+    var newUser     = new User();
+    var datetime    = new Date();
+        
+   console.log("parsing registration form");
+    form.parse(req, function (req,fields, err){
+        
+        // username    = fields.username;
+        // email       = fields.email;
+        // password    = fields.password;
+        // repassword  = fields.repassword;
+
+        // output of data in form fields.
+        console.log("username :: " + fields.username); 
+        console.log("email :: " + fields.email);
+        console.log("password :: " + fields.password); 
+        console.log("password retype ::  " + fields.repassword); 
+
+        // build data object to be persisted.
+        newUser.name        = fields.username;
+        newUser.email       = fields.email;
+        newUser.password    = fields.password;
+        newUser.joinedDate  = datetime;
+
+        
+    });
+
+    console.log(" \npersisting data to database . . .");
+
+    newUser.save(function(err, user){
+        if(err){
+            console.log('\n ... error saving user data ...');
+        }else{
+            res.send(newUser);
+            console.log('\n data persisted . . .');
+        }
+    });
+
+    //res.end("user registered");
+});
+
+
+// get all users registered in the system.
+app.get('/users', function(req,res){
+
+    console.log("retreiving users registered on system..");
+
+    User.find({})
+        .exec(function(err, users){
+            if(err){
+                res.send("error occured on server");
+            } else {
+                console.log(users);
+                res.json(users);
+            }
+        });
+
+
+
+    //res.send("happy to be here ");
+
+
+});
+
+
+
+
+
+
+
+
+
+
 
 //app.use(formidable.parse());
 
@@ -67,38 +174,18 @@ app.post('/login',function(req,res){
       res.end("logged in");
 });
 
-app.get('/register',function(req,res){
-      res.sendFile(__dirname + "/register.html");
-});
 
-app.post('/register',function(req,res){
 
-    var form = new formidable.IncomingForm();
-    var username, email, password;
 
-   // form.parse(req,);
 
-    form.parse(req, function (req,fields, err){
-        username    = fields.username;
-        email       = fields.email;
-        password    = fields.password;
-
-        console.log("cattype = " + fields.username); 
-        console.log("filename = " + fields.email);
-        console.log("filename = " + fields.password); 
-    });
-
-    res.end("user registered");
-});
 
 app.get('/hello',function(req,res){
+
+        //User.collection.drop();
       res.sendFile(__dirname + "/hello.html");
 });
 
 
-var listener = app.listen(3000, function(){
-	console.log('listening on port ' + listener.address().port);
-});
 
 
 
